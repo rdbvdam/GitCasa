c9d575aade5e223d5a35e3ce908c557a45c6d0c5
Ricardo <ricardo.belvel@iesjulianmarias.es>
2026-06-07 17:38:21 +0200

    Probando el gitignore

---
+++ src/tanda2/Tanda2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tanda2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tanda2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**
+         * E- Ingéniatelas para que la carpeta build no se gestione con Git.
+         * Para comprobarlo, ejecuta el proyecto y tras generarse build mira en
+         * el status si hay cambios pendientes relativos a esa carpeta. F- Ahora
+         * crea una clase nueva, la que tú quieras. Haz una revisión. G- Mira tu
+         * área de trabajo (el proyecto en NetBeans, con las carpetas source)
+         * correspondientes. En ese momento, vuelve momentáneamente a la
+         * revisión inicial. ¿Desaparece lo nuevo? ¿Hay información sobre las
+         * revisiones posteriores, o se ha perdido? Vuelve de nuevo a la
+         * revisión inicial (con el nombre de la rama puedes). H- Ahora crea un
+         * repositorio en GitHub y sube tu proyecto (tu rama local con su mismo
+         * nombre). ¿Qué contenidos ves y cuáles no allí? I- Haz una
+         * modificación en el Readme de GitHub en esa rama más evolucionada.
+         * Luego baja el proyecto a local (a Git). ¿Qué ves?
+         * 
+         * 
+         */
+        
+        
+        
+        System.out.println("Probando el gitignore");
+    }
+    
+}
