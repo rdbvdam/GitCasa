4e93d0b3b40b43de20c6e6f072fb359698958024
Ricardo <ricardo.belvel@iesjulianmarias.es>
2026-06-07 17:45:38 +0200

    Revision de la clase persona

---
+++ src/tanda2/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tanda2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Persona {
+private String nombre;
+private int edad;
+    
+}
--- src/tanda2/Tanda2.java
+++ src/tanda2/Tanda2.java
@@ -18,13 +18,16 @@ public class Tanda2 {
         /**
          * E- Ingéniatelas para que la carpeta build no se gestione con Git.
          * Para comprobarlo, ejecuta el proyecto y tras generarse build mira en
-         * el status si hay cambios pendientes relativos a esa carpeta. F- Ahora
-         * crea una clase nueva, la que tú quieras. Haz una revisión. G- Mira tu
+         * el status si hay cambios pendientes relativos a esa carpeta. 
+         * F- Ahora
+         * crea una clase nueva, la que tú quieras. Haz una revisión. 
+         * G- Mira tu
          * área de trabajo (el proyecto en NetBeans, con las carpetas source)
          * correspondientes. En ese momento, vuelve momentáneamente a la
          * revisión inicial. ¿Desaparece lo nuevo? ¿Hay información sobre las
          * revisiones posteriores, o se ha perdido? Vuelve de nuevo a la
-         * revisión inicial (con el nombre de la rama puedes). H- Ahora crea un
+         * revisión inicial (con el nombre de la rama puedes). 
+         * H- Ahora crea un
          * repositorio en GitHub y sube tu proyecto (tu rama local con su mismo
          * nombre). ¿Qué contenidos ves y cuáles no allí? I- Haz una
          * modificación en el Readme de GitHub en esa rama más evolucionada.
