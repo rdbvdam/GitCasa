91ac113cbdb8cd91e4877064b6fac58f3755bfa4
Ricardo <ricardo.belvel@iesjulianmarias.es>
2026-06-07 17:06:59 +0200

    Revision inicial

---
+++ src/tanda1/EjercicicosEntornos.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tanda1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjercicicosEntornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /**
+         * A- Una primera versión sin realizar modificaciones, llámala revisión
+         * inicial.
+         * B- Ve introduciendo 3 comentarios diferentes en el .java.
+         * Cada vez que introduzcas uno nuevo haz una revisión que se llame
+         * "Revisión 1... 2... 3", dependiendo del caso.
+         * C- Visualiza la
+         * información de todas las revisiones. 
+         * D- Visualiza las diferencias
+         * entre la revisión más avanzada y cada una de las que has ido
+         * haciendo.
+         */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
